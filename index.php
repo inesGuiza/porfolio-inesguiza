0c5cfd5f6698e7efe96c0a2eee9f26b925a3c8cc
unknown <ins.guiza@gmail.com>
2019-04-05 14:08:13 +0200

    création de mon templte, structure de base + htlm/structure de mon portfolio de base, installation de bootstrap, structur'e scss, création des fichiers index.php, header.php et footer.php

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="ines" content="">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Portfolio Ines Guiza</title>
+</head>
+<body>
+<!-- PAGE 1 // ACCEUIL --> 
+<header class="bg-dark"> 
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-dark">
+      <a class="name" href="./">Inès GUIZA</a>
+      <!-- menu -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+
+</header>
+
+<main class="bg-dark">
+    <!-- Section Présentation -->
+    <div class="section">
+      <div class="full-page-1">
+        <!-- left side -->
+        <div class="title title-top">
+          <h1>Inès Guiza</h1>
+          <div></div>
+          <p>Junior Architect / <br> freelance Full Stack <br> Web Developer.</p>
+          <p>Living & working in Brussels.</p>
+
+        </div>
+        <!-- right side -->
+        <div class="dynamique">
+          <div>
+            <p>Portfolio</p>
+            <p class="quote">" Qui ne progresse pas chaque jour, regresse chaque jour." <span>Confucius</span>
+          </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  <!-- menu déroulant -->
+<div id="barres-nav" class="left">
+  <ul>
+    <li>
+      <a href="#top" class="active">
+      <span ></span>
+      </a>
+    </li>
+    <li>
+      <a href="#portfolio" class="">
+        <span></span>
+      </a>
+    </li>
+    <li>
+      <a href="#about" class="">
+        <span ></span>
+      </a>
+    </li>
+    <li>
+      <a href="#contact" class="">
+        <span ></span>
+      </a>
+    </li>
+  </ul>
+</div>
+   
+
+<!-- SECTION PORTFOLIO -->
+<div class="section">
+  <div class="fullpage__slide">
+    <!-- architecture -->
+      <!-- texte & buton link-->
+    <div class="title__demi">
+      <h2 class="title__text__demi js-letter">University<br>PROJECTS</h2>
+      <div class="border js-letter"><span></span></div>
+      <p class="title__lead js-letter">architecture</p>
+      <div class="btn-wrap js-letter">
+        <a class="btn" href="./portfolio-architecture.html"> Show me more</a>
+      </div>
+    </div>
+      <!-- image link -->
+    <a class="image2 image_myworkarchitecture" href="/portfolio-architecture.html">
+      <div class="image__over2">
+        <div class="image__cover2"></div>
+        <div class="image__cover2"></div>
+      </div>
+    </a>
+    <!-- webdesign -->
+      <!-- texte & buton link-->
+    <div class="title__demi">
+      <h2 class="title__text__demi js-letter">Coding<br>PROJECTS</h2>
+      <div class="border js-letter"><span></span></div>
+      <p class="title__lead js-letter">webdesign</p>
+      <div class="btn-wrap js-letter">
+        <a class="btn" href="./portfolio-webdesign.html"> Show me more</a>
+      </div>
+    </div>
+      <!-- image link -->
+    <a class="image2 image_myworkwebdesign" href="/portfolio-webdesign.html">
+      <div class="image__over2">
+        <div class="image__cover2"></div>
+        <div class="image__cover2"></div>
+      </div>
+    </a>
+    <!-- number page 01 -->
+    <div class="page-num">
+      <p>01</p>
+    </div>
+  </div>
+</div>
+
+
+<!-- SECTION ABOUT ME  -->
+<div class="section">
+  <div class="fullpage__slide">
+    <!-- texte & buton link-->
+    <div class="title">
+      <h2 class="title__text js-letter">About Me</h2>
+      <div class="border js-letter"><span></span></div>
+      <p class="title__lead js-letter">I love and I believe in Communication, Design and Travel.  </p>
+      <div class="btn-wrap js-letter">
+        <a class="btn" href="./about.html"> Show me more</a>
+      </div>
+    </div>
+    <!-- image link -->
+    <a class="image image_about" href="/about.html">
+      <div class="image__over">
+        <div class="image__cover"></div>
+        <div class="image__cover"></div>
+      </div>
+    </a>
+    <!-- number page 02 -->
+    <div class="page-num">
+      <p>02</p>
+    </div>
+
+  </div>
+</div>
+<!-- SECTION GET IN TOUcH -->
+<div class="section">
+  <div class="fullpage__slide">
+    <!-- texte & buton link-->
+    <div class="title">
+      <h2 class="title__text js-letter">Get In Touch</h2>
+      <div class="border js-letter">
+        <span></span>
+      </div>
+      <ul>
+        <li class="js-letter"><a href="mailt0:ins.guiza@gmail.com">ins.guiza@gmail.com</a>
+        </li>
+        <li class="js-letter"> 
+          <a href="https://twitter.com/Ines_gza" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.instagram.com/ines_gza/?hl=fr" target="_blank"><i class="fab fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+      <!-- image link -->
+      <a class="image image_contact" href="/about.html">
+      <div class="image__over">
+        <div class="image__cover"></div>
+        <div class="image__cover"></div>
+      </div>
+    </a>
+    <!-- number page 03 -->
+    <div class="page-num">
+      <p>03</p>
+    </div>
+  </div>
+</div>
+<p class="scrollDown">SCROLL</p>
+</main>
+<!-- SECTION FOOTER -->
+<footer>
+
+</footer>
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
