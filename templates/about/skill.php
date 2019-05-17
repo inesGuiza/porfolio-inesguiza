43f2e457ef7d7473d2b74c33efd7e560f3658de7
unknown <ins.guiza@gmail.com>
2019-05-17 14:06:57 +0200

    section skill OK, amélioration de la section passions

--- templates/about/skill.php
+++ templates/about/skill.php
@@ -2,22 +2,59 @@
             <div class="fullpage_slide container-fluide">
                 <div class="row">
                     <p class="subtitle-about">
-                    03
+                        03
                     </p>
                     <h3 class="title-about">Skill Set
                         <div class="border-yellow-about js-letter">
-                                <span class="barre1-about"></span>
-                        </div> 
-                    </h3>  
+                            <span class="barre1-about"></span>
+                        </div>
+                    </h3>
                 </div>
-                <div class="col-6">
-                    <h4>Inès Guiza</h4>
-                    <p></p>
-                </div>
-                <div class="col-6">
-                    <h4>Inès Guiza</h4>
-                    <p></p>
+                <div class="row">
+                    <div class="col-5 mx-auto ">
+                        <div class="skills-container ">
+
+                            <!-- à droite -->
+                            <div class="skills-design">
+
+                                <h5 class="skills-title">graphic design</h5>
+                                <p class="skills-body">photographie • color <br> typography • identity <br> presentation
+                                    <br>
+                                </p>
+                                <strong class="programmes">Adobe Photoshop <br> Adobe Indesign <br> Adobe Illustrator <br> Adobe Premiere Pro</strong>
+                            </div>
+                            <!-- à gauche   -->
+                            <div class="skills-archi">
+
+                                <h5 class="skills-title">architecture</h5>
+                                <p class="skills-body">maquette • dessin
+                                    <br>
+                                </p>
+                                <strong class="programmes">Autocad <br> Sketch Up <br> Revit (objectif apprentissage)</strong>
+
+                            </div>
+                            <!-- en bas à droite -->
+                            <div class="skills-gestion">
+
+                                <h5 class="skills-title">gestion</h5>
+                                <p class="skills-body">communication • organisation <br>objectifs • planning
+                                    <br>
+                                </p>
+                                <strong class="programmes">StackEdit <br> Slack <br> Trello <br> Suite Office</strong>
+
+                            </div>
+                            <!-- en bas à gauche -->
+                            <div class="skills-web">
+
+                                <h5 class="skills-title">web dev</h5>
+                                <p class="skills-body">html • css/sass <br>javascript • git <br>emmet • bootstrap
+                                    <br>
+                                </p>
+                                <strong class="programmes"> Wordpress <br> gitHub</strong>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-</main>
+        </main>
