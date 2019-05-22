778b8d42ff8b7d8d022452397bb87a53951705e1
unknown <ins.guiza@gmail.com>
2019-05-22 16:21:43 +0200

    citations correction style

--- templates/about/passion.php
+++ templates/about/passion.php
@@ -17,20 +17,23 @@
                 <h2 class="title-passion text-center"><strong>Architecture</strong> </h2>
 
                 <p class="text-center passions">L'architecture est un moyen puissant d'émancipation et de bien-être dans les paysages urbains et ruraux. <br> Des architectures justes, humaines, équilibrées, innovantes et conscientes des ressources réelles qu'offrent la Terre doivent être au centre de nos préoccupations, à mon humble avis.</p>
-                <p class="text-center citations mt-3">"Vers une architecture durable, à l'échelle humaine et qui s'intègre dans un environnement qui respecte et intégre chaque individu dans une société démocratique."</p>
+                <span class="guill">‘​‌‘​‌</span>
+                <p class="text-center citations">Vers une architecture durable, à l'échelle humaine et qui s'intègre dans un environnement qui respecte et intégre chaque individu dans une société démocratique.</p>
             </div>
             <div class="col-4 text-center">
                 <i class="fas fa-laptop fa-3x mt-4"></i>
                 <h2 class="title-passion text-center"><strong> Web Development</strong></h2>
                 <p class="text-center passions">Savoir communiquer est un art et s'apprend. Dans le monde du design, un portfolio est une arme pour faire valoir et faire découvrir son travail. Pour moi, il a été très vite question d'apprendre le web development pour avoir plus d'un tour dans mon sac et faire mon propre site. C'est un domaine complexe qui demande beaucoup de rigueur et de l'auto-discipline dans l'apprentissage.</p>
-                <p class="text-center citations mt-3"> " Internet est un outil puissant, être présent sur les réseaux en mettant en lumière son travail et son investissement professionnel est une priorité dans un monde qui change et évolue chaque jour."</p>
+                <span class="guill">‘​‌‘​‌</span>
+                <p class="text-center citations mt-3">  Internet est un outil puissant, être présent sur les réseaux en mettant en lumière son travail et son investissement professionnel est une priorité dans un monde qui change et évolue chaque jour.</p>
             </div>
             <div class="col-4 text-center">
                 <i class="fas fa-globe-europe fa-3x mt-4"></i>
                 <h2 class="title-passion text-center"><strong> Travel & Guide-Tour</strong></h2>
                 <p class="text-center passions">Déambuler dans le monde, l'observer, l'écouter, le découvrir est pour moi, le moyen le plus puissant d'émancipation. Il permet d'avoir une lecture humble du monde et est une source d'inspiration sans cesse riche et variée qui permet de garder un oeil vif. Le voyage, selon moi, permet de rester plus humain, plus respectueux de la nature et en harmonie avec elle. Une nécessité.</p>
+                <span class="guill">‘​‌‘​‌</span>
                 <p class="text-center citations mt-3">
-                    "Voyager est une passion, cependant partager et promouvoir l'est encore plus. C'est pour ces raisons que j'aspire à continuer à découvrir et m'émerveiller devant la beauté du monde.. accompagnée par toutes les belles âmes qui le désirent."
+                    Voyager est une passion, cependant partager et promouvoir l'est encore plus. C'est pour ces raisons que j'aspire à continuer à découvrir et m'émerveiller devant la beauté du monde.. accompagnée par toutes les belles âmes qui le désirent.
                 </p>
             </div>
         </div>
